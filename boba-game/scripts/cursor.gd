e44af3a939cbfa0f6a3a65ebfcf390f97c2c8798
Michael <ch.michael085@gmail.com>
2025-02-12 14:33:51 -0800

    custom cursor

---
+++ boba-game/scripts/cursor.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+var default_cursor = load("res://assets/cursors/hand_point.png")
+
+func _ready():
+	Input.set_custom_mouse_cursor(default_cursor, 0, Vector2(0, 0)) 
---
+++ boba-game/scripts/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_play_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	pass # Replace with function body.
